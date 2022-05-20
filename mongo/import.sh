a1abb250f8ceb1f5553070971016b9da528eaff8
YuqiHuai <yhuai@uci.edu>
2022-05-19 23:58:15 -0700

    fix: initialize mongodb appropriately

---
+++ mongo/import.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+mongoimport --host localhost --db WISE --collection global --type json --file /docker-entrypoint-initdb.d/global.json --jsonArray
+mongoimport --host localhost --db WISE --collection maps --type json --file /docker-entrypoint-initdb.d/maps.json --jsonArray
+mongoimport --host localhost --db WISE --collection plugins --type json --file /docker-entrypoint-initdb.d/plugins.json --jsonArray
+mongoimport --host localhost --db WISE --collection vehicles --type json --file /docker-entrypoint-initdb.d/vehicles.json --jsonArray
