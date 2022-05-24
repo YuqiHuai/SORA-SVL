8608a8b92fb444c91391c10a4900241b3009a609
YuqiHuai <yhuai@uci.edu>
2022-05-24 00:44:26 -0700

    mongo: added hd_map collection

--- mongo/import.sh
+++ mongo/import.sh
@@ -4,3 +4,4 @@ mongoimport --host localhost --db WISE --collection global --type json --file /d
 mongoimport --host localhost --db WISE --collection maps --type json --file /docker-entrypoint-initdb.d/maps.json --jsonArray
 mongoimport --host localhost --db WISE --collection plugins --type json --file /docker-entrypoint-initdb.d/plugins.json --jsonArray
 mongoimport --host localhost --db WISE --collection vehicles --type json --file /docker-entrypoint-initdb.d/vehicles.json --jsonArray
+mongoimport --host localhost --db WISE --collection hd_maps --type json --file /docker-entrypoint-initdb.d/hd_maps.json --jsonArray
