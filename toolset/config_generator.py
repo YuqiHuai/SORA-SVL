3154e6641a7ae3f29173ed28c380400a29902eb3
YuqiHuai <yhuai@uci.edu>
2022-06-27 16:34:20 -0700

    toolset: added db utilities

--- toolset/config_generator.py
+++ toolset/config_generator.py
@@ -1,133 +1,110 @@
 # Generate Sensor Configuration from JSON
 import json
-import os
-import sys
 from datetime import datetime
 import argparse
+from db_utils import WiseDB
+
 
 class config_generator(object):
 
     def __init__(self, vehicle_id, bridge_id):
-        
+        self.db = WiseDB()
         self.vehicle_id = vehicle_id
         self.bridge_id = bridge_id
-        self.bridge_asset = None
-        self.vehicle_asset = None
 
-        plugins_path = '../mongo/plugins.json'
-        with open(plugins_path, 'r+') as f:
-            plugins_log  = json.load(f)
-        self.plugin_id2guid = dict()
-        self.plugin_guid2id = dict()
+        self.vehicle_asset = self.db.get_vehicle_by_id(self.vehicle_id)
+        assert self.vehicle_asset, 'Vehicle does not exist'
+        assert self.vehicle_asset['ctype'] == 'vehicle', 'Not a base vehicle'
 
-        for plugin in plugins_log:
-            plugin_data = plugin['data']
-            self.plugin_id2guid[plugin['cid']] = plugin_data['assetGuid']
-            self.plugin_guid2id[plugin_data['assetGuid']] = plugin['cid']
-            if plugin['cid'] == bridge_id:
-                self.bridge_asset = plugin
+        self.bridge_asset = self.db.get_plugin_by_id(self.bridge_id)
+        assert self.bridge_asset, 'Bridge does not exist'
+        assert self.bridge_asset['data']['category'] == 'bridge', 'Not a bridge'
 
-        vehicles_path = '../mongo/vehicles.json'
-        with open(vehicles_path, 'r+') as f:
-            vehicles_log  = json.load(f)
-        self.config_template = dict()
-        self.vehicle_id2guid = dict()
-        self.vehicle_guid2id = dict()
-
-        for vehicle in vehicles_log:
-            try:
-                if vehicle['ctype'] == 'vehicle':
-                    vehicle_data = vehicle['data']
-                    self.vehicle_id2guid[vehicle['cid']] = vehicle_data['assetGuid']
-                    self.vehicle_guid2id[vehicle_data['assetGuid']] = vehicle['cid']
-                    if vehicle['cid'] == vehicle_id:
-                        self.vehicle_asset = vehicle
-            except(KeyError):
-                continue
-
-        for vehicle in vehicles_log:
-            if vehicle['ctype'] == 'configuration':
-                self.config_template = vehicle
-                break
-    
-    def generate_config(self, sensor_config_fp, cid, name):
+    def generate_config(self, sensor_config_fp, name):
 
         now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
         with open(sensor_config_fp, 'r') as f:
             sensor_config = json.load(f)
 
-        self.config_template['name']     = name
-        self.config_template['cid']      = cid
-        self.config_template['endpoint'] = '/api/v1/vehicles/{}'.format(cid)
-        self.config_template['data']['assetGuid']    = self.vehicle_id2guid[self.vehicle_id]
-        self.config_template['data']['id']           = self.vehicle_id
-        self.config_template['data']['name']         = self.vehicle_asset['data']['name']
-        self.config_template['data']['ownerId']      = self.vehicle_asset['data']['ownerId']
-        self.config_template['data']['accessType']   = self.vehicle_asset['data']['accessType']
-        self.config_template['data']['imageUrl']     = self.vehicle_asset['data']['imageUrl']
-        self.config_template['data']['status']       = self.vehicle_asset['data']['status']
-        self.config_template['data']['tags']         = self.vehicle_asset['data']['tags']
-        self.config_template['data']['owner']        = self.vehicle_asset['data']['owner']
-        self.config_template['data']['shareRequests']= self.vehicle_asset['data']['shareRequests']
-        self.config_template['data']['favoredBy']    = self.vehicle_asset['data']['favoredBy']
-        self.config_template['data']['sharedWith']   = self.vehicle_asset['data']['sharedWith']
-        self.config_template['data']['createdAt']     = now
-        self.config_template['data']['updatedAt']     = now
-        self.config_template['data']['bridge']        = dict()
-        try:
-            self.config_template['data']['bridge']['id']                 = self.bridge_asset['data']['id']
-            self.config_template['data']['bridge']['name']               = self.bridge_asset['data']['name']
-            self.config_template['data']['bridge']['assetGuid']          = self.bridge_asset['data']['assetGuid']
-            self.config_template['data']['bridge']['type']               = self.bridge_asset['data']['type']
-        except(KeyError, TypeError):
-            self.config_template['data']['bridge']    = None
-        self.config_template['data']['sensors']       = []
+        attributes = ['assetGuid', 'isShared', 'isFavored', 'isOwned', 'accessInfo', 'supportedSimulatorVersions', 'id', 'name', 'description', 'copyright',
+                      'licenseName', 'ownerId', 'accessType', 'imageUrl', 'status', 'tags', 'owner', 'baseLink']
+
+        cid = self.db.generate_id_for_vehicle()
+
+        config_template = dict()
+        config_template['name'] = name
+        config_template['cid'] = cid
+        config_template['ctype'] = "configuration"
+        config_template['endpoint'] = '/api/v1/vehicles/{}'.format(cid)
+        config_template['data'] = dict()
+        for k in attributes:
+            config_template['data'][k] = self.vehicle_asset['data'][k]
+
+        config_template['data']['bridge'] = self.bridge_asset['data']
+
+        config_template['data']['sensors'] = []
         for sensor in sensor_config:
             sensor_dict = dict()
             sensor_id = sensor['pluginId']
-            try :
-                asset_guid = self.plugin_id2guid[sensor_id]
-                sensor_dict['name']                = sensor['name']
-                sensor_dict['parent']              = sensor['parent']
-                sensor_dict['type']                = sensor['type']
-                sensor_dict['plugin']              = dict()
-                sensor_dict['plugin']['id']        = sensor_id
-                sensor_dict['plugin']['type']      = sensor['plugin']['type']
+            try:
+                asset_guid = self.db.get_plugin_assetGuid_by_id(sensor_id)
+                assert asset_guid
+                sensor_dict['name'] = sensor['name']
+                sensor_dict['parent'] = sensor['parent']
+                sensor_dict['type'] = sensor['type']
+                sensor_dict['plugin'] = dict()
+                sensor_dict['plugin']['id'] = sensor_id
+                sensor_dict['plugin']['type'] = sensor['plugin']['type']
                 sensor_dict['plugin']['assetGuid'] = asset_guid
                 try:
-                    sensor_dict['params']    = sensor['params']
+                    sensor_dict['params'] = sensor['params']
                 except(KeyError):
                     print("No Params for Sensor: {}".format(sensor['name']))
                 try:
-                    sensor_dict['transform']           = sensor['transform']
+                    sensor_dict['transform'] = sensor['transform']
                 except(KeyError):
                     print("No Transform for Sensor: {}".format(sensor['name']))
-                self.config_template['data']['sensors'].append(sensor_dict)
-            except(KeyError):
+                config_template['data']['sensors'].append(sensor_dict)
+            except(AssertionError):
                 asset_guid = 'Sensor not in MongoDB'
-                print(" Sensor Name: {}, Sensor ID: {}, Asset GUID: {}".format(sensor['type'], sensor_id,asset_guid))
+                print(" Sensor Name: {}, Sensor ID: {}, Asset GUID: {}".format(
+                    sensor['type'], sensor_id, asset_guid))
 
-        with open("{}.json".format(cid), 'w') as f:
-            config_string = json.dump(self.config_template, f, indent = 4)
+        # with open("{}.json".format(cid), 'w') as f:
+        #     config_string = json.dump(config_template, f, indent=4)
+
+        self.db.get_collection('vehicles').insert_one(config_template)
+        print(cid)
+
+
+def testMain():
+    # Lincoln2017MKZ
+    # 73805704-1e46-4eb6-b5f9-ec2244d5951e
+    # cyberRT Bridge
+    # ff4ba32f-8885-4847-9a8b-fd21e6603202
+    config_gen = config_generator(
+        '73805704-1e46-4eb6-b5f9-ec2244d5951e', 'ff4ba32f-8885-4847-9a8b-fd21e6603202')
+    config_gen.generate_config('./sample_data/s1.json', 'JasonBourne')
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--vehicle_id', type=str, help='vehicle cid')
-    parser.add_argument('--bridge_id', type=str, help='vehicle cid')
-    parser.add_argument('--sensor_config', type=str, help='name of configuration file')
-    parser.add_argument('--config_name', type=str, help='name of configuration')
-    parser.add_argument('--cid', type=str, help='unique id for database')
-    
+    parser.add_argument('--bridge_id', type=str, help='bridge cid')
+    parser.add_argument('--sensor_config', type=str,
+                        help='name of configuration file')
+    parser.add_argument('--config_name', type=str,
+                        help='name of configuration')
+
     args = parser.parse_args()
     vehicle_id = args.vehicle_id
     bridge_id = args.bridge_id
     sensor_config_fp = args.sensor_config
-    cid_generator = args.cid ## TBD: generate unique cid
     config_name = args.config_name
 
     config_gen = config_generator(vehicle_id, bridge_id)
-    config_gen.generate_config(sensor_config_fp, cid_generator, config_name)
+    config_gen.generate_config(sensor_config_fp, config_name)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
-    
---
+++ toolset/db_utils.py
@@ -0,0 +1,61 @@
+import pymongo
+from uuid import uuid4
+
+
+class WiseDB:
+    def __init__(self) -> None:
+        self.client = pymongo.MongoClient('mongodb://localhost:27017')
+        self.db = self.client['WISE']
+
+    def get_collection(self, collection_name: str):
+        return self.db[collection_name]
+
+    def _get_x_by_id(self, x: str, x_id: str):
+        assert x in ['maps', 'vehicles', 'plugins']
+        return self.get_collection(x).find_one({'cid': x_id})
+
+    def _get_x_assetGuid_by_id(self, x: str, x_id: str):
+        obj = self._get_x_by_id(x, x_id)
+        if obj:
+            return obj['data']['assetGuid']
+        return None
+
+    def get_map_by_id(self, map_id: str):
+        return self._get_x_by_id('maps', map_id)
+
+    def get_map_assetGuid_by_id(self, map_id: str):
+        return self._get_x_assetGuid_by_id('maps', map_id)
+
+    def get_vehicle_by_id(self, vehicle_id: str):
+        return self._get_x_by_id('vehicles', vehicle_id)
+
+    def get_vehicle_assetGuid_by_id(self, vehicle_id: str):
+        return self._get_x_assetGuid_by_id('vehicles', vehicle_id)
+
+    def get_plugin_by_id(self, plugin_id: str):
+        return self._get_x_by_id('plugins', plugin_id)
+
+    def get_plugin_assetGuid_by_id(self, plugin_id: str):
+        return self._get_x_assetGuid_by_id('plugins', plugin_id)
+
+    def _generate_id_for_x(self, x: str):
+        assert x in ['maps', 'vehicles', 'plugins']
+        while True:
+            new_id = str(uuid4())
+            if self._get_x_by_id(x, new_id):
+                continue
+            return new_id
+
+    def generate_id_for_map(self):
+        return self._generate_id_for_x('maps')
+
+    def generate_id_for_vehicle(self):
+        return self._generate_id_for_x('vehicles')
+
+    def generate_id_for_plugin(self):
+        return self._generate_id_for_x('plugins')
+
+
+if __name__ == '__main__':
+    db = WiseDB()
+    print(db.generate_id_for_map())
