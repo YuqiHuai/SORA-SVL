25e079914b81fa561f46de589962705c1e8d2db9
YuqiHuai <yhuai@uci.edu>
2022-06-29 22:11:35 -0700

    restructure mongo container
    - You can now access mongo from outside using port 28018 instead of 27017 to avoid conflicts with existing mongo service
    - mongo database is now persistent, restarting will not clear added data
    - to erase added data, remove all files under /mongo/data except .gitkeep

--- mongo/import.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-mongoimport --host localhost --db WISE --collection global --type json --file /docker-entrypoint-initdb.d/global.json --jsonArray
-mongoimport --host localhost --db WISE --collection maps --type json --file /docker-entrypoint-initdb.d/maps.json --jsonArray
-mongoimport --host localhost --db WISE --collection plugins --type json --file /docker-entrypoint-initdb.d/plugins.json --jsonArray
-mongoimport --host localhost --db WISE --collection vehicles --type json --file /docker-entrypoint-initdb.d/vehicles.json --jsonArray
-mongoimport --host localhost --db WISE --collection hd_maps --type json --file /docker-entrypoint-initdb.d/hd_maps.json --jsonArray
---
+++ mongo/setup/import.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+mongoimport --host localhost --db WISE --collection global --mode=upsert --type json --file /docker-entrypoint-initdb.d/global.json --jsonArray
+mongoimport --host localhost --db WISE --collection maps --mode=upsert --type json --file /docker-entrypoint-initdb.d/maps.json --jsonArray
+mongoimport --host localhost --db WISE --collection plugins --mode=upsert --type json --file /docker-entrypoint-initdb.d/plugins.json --jsonArray
+mongoimport --host localhost --db WISE --collection vehicles --mode=upsert --type json --file /docker-entrypoint-initdb.d/vehicles.json --jsonArray
+mongoimport --host localhost --db WISE --collection hd_maps --mode=upsert --type json --file /docker-entrypoint-initdb.d/hd_maps.json --jsonArray
