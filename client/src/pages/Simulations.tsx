3f5732bf2bf0404d0e2d0749f6d143fab08620c0
Sanggu Han <korean139@gmail.com>
2022-08-19 01:14:27 +0900

    fix(client): fetch on page mount

--- client/src/pages/Simulations.tsx
+++ client/src/pages/Simulations.tsx
@@ -24,6 +24,7 @@ function Simulations() {
       setSimulatorStatus(simulatorStatus as SimulatorStatusMap);
     };
 
+    fetchSimulatorStatus();
     const updateSimulatorStatus = setInterval(() => {
       fetchSimulatorStatus();
     }, 500);
