ba1120696d7103afa88913502a9ccb669448b7c8
Haifeng Zhao <hf_zhao@yeah.net>
2023-01-20 22:42:25 +0800

    + fix the bug for VSE failed to load map.(can load map and vehicles).

--- server/src/routers/api.router.ts
+++ server/src/routers/api.router.ts
@@ -1,10 +1,46 @@
+import { log } from "console";
 import express from "express";
+import { Long } from "mongodb";
 import { collections } from "../services/database.service";
 export const apiRouter = express.Router();
 
 apiRouter.get("/maps", async (req, res) => {
-  const maps = await collections.global.findOne({ type: "maps" });
-  res.status(200).send(maps.data);
+  log('request=------------------%s', req.query);
+  let display = req.query.display;
+  let limit = req.query.limit;
+  let offset = req.query.offset;
+  log('display=%s,limit=%s,offset=%s', display, typeof (limit), typeof (offset));
+
+  if (display != null) {
+    var count = await collections.maps.aggregate([{ "$count": "count" }]).toArray()
+    // count.forEach(function(doc) {
+    //   log('debug: doc=%s',doc);
+    // })
+    log("debug: count=%s+++++++", JSON.stringify(count));
+    var maps = await collections.maps.aggregate([{ "$replaceWith": { rows: "$data" } },
+    {
+      $project: {
+        "rows.lastVersionDate": 0, "rows.lastAssetGuid": 0, "rows.supportedPlatforms": 0, "rows.version": 0, "rows.authorName": 0, "rows.authorUrl": 0,
+        "rows.createdAt": 0, "rows.updatedAt": 0, "rows.tags": 0
+      }
+    },
+    { $addFields: { "rows.shareRequests": [] } },
+    {
+      $group: {
+        "_id": null,
+        "rows": { "$firstN": { "input": "$rows", n: 150 } },
+        "count": { "$count": {} }
+      }
+    },
+    { $project: { "_id": 0, "count": 1, "rows": 1 } }
+    ]).toArray();
+    log("debug:maps=-------:%s+++++", JSON.stringify(maps[0]));
+    res.status(200).send(maps[0]);
+  } else {
+
+    const maps = await collections.global.findOne({ type: "maps" });
+    res.status(200).send(maps.data);
+  }
 });
 
 apiRouter.get("/maps/:id", async (req, res) => {
@@ -40,8 +76,45 @@ apiRouter.get("/plugins/:id", async (req, res) => {
 });
 
 apiRouter.get("/vehicles", async (req, res) => {
-  const vehicles = await collections.global.findOne({ type: "vehicles" });
-  res.status(200).send(vehicles.data);
+  log('request=------------------%s', req.query);
+  let display = req.query.display;
+  let limit = req.query.limit;
+  let offset = req.query.offset;
+  log('/vehicles: display=%s,limit=%s,offset=%s', display, typeof (limit), typeof (offset));
+
+  if (display != null) {
+    var count = await collections.vehicles.aggregate([{ "$match": { "ctype": "vehicle" } }, { "$count": "count" }]).toArray()
+    // count.forEach(function(doc) {
+    //   log('debug: doc=%s',doc);
+    // })
+    var vehicle_count = count[0].count;
+
+    log("debug: vechiles: count=%s+++++++", JSON.stringify(count));
+    var vehicles = await collections.vehicles.aggregate([
+      { "$match": { "ctype": "vehicle" } },
+      { "$replaceWith": { rows: "$data" } },
+      {
+        $project: {
+          "rows.lastVersionDate": 0, "rows.lastAssetGuid": 0, "rows.supportedPlatforms": 0, "rows.version": 0, "rows.authorName": 0, "rows.authorUrl": 0,
+          "rows.createdAt": 0, "rows.updatedAt": 0, "rows.tags": 0, "rows.fmu": 0, "rows.baseLink": 0, "rows.sensorsConfigurations.owner": 0
+        }
+      },
+      { $addFields: { "rows.shareRequests": [], "rows.sensorsConfigurations.sharedWith": [] } },
+      {
+        $group: {
+          "_id": null,
+          "rows": { "$firstN": { "input": "$rows", n: vehicle_count } },
+          "count": { "$count": {} }
+        }
+      },
+      { $project: { "_id": 0, "count": 1, "rows": 1 } }
+    ]).toArray();
+    log("debug:vechiles:=-------:%s+++++", JSON.stringify(vehicles[0]));
+    res.status(200).send(vehicles[0]);
+  } else {
+    const vehicles = await collections.global.findOne({ type: "vehicles" });
+    res.status(200).send(vehicles.data);
+  }
 });
 
 apiRouter.get("/vehicles/:id", async (req, res) => {
