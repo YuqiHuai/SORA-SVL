e6ee0ea31a3990571285bfdd4648304c185f5775
Y <yhuai@uci.edu>
2023-10-17 12:44:02 -0700

    fix(client): proxy address

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "/",
+    "/api",
     createProxyMiddleware({
       target: process.env.REACT_APP_SERVER_URL,
       changeOrigin: true,
