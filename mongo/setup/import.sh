1cc74cd22e207b7834eb112af91564b4c3d72fd6
elgeeko1 <11233838+elgeeko1@users.noreply.github.com>
2024-12-02 19:55:52 -0800

    Add third-party licenses to mongo image. Add WISE geojson asset to mongo image.

--- mongo/setup/import.sh
+++ mongo/setup/import.sh
@@ -5,4 +5,4 @@ mongoimport --host localhost --db WISE --collection maps --mode=upsert --type js
 mongoimport --host localhost --db WISE --collection plugins --mode=upsert --type json --file /docker-entrypoint-initdb.d/plugins.json --jsonArray
 mongoimport --host localhost --db WISE --collection vehicles --mode=upsert --type json --file /docker-entrypoint-initdb.d/vehicles.json --jsonArray
 mongoimport --host localhost --db WISE --collection hd_maps --mode=upsert --type json --file /docker-entrypoint-initdb.d/hd_maps.json --jsonArray
-# mongoimport --host localhost --db WISE --collection geojson --mode=upsert --type json --file /docker-entrypoint-initdb.d/geojson.json --jsonArray
+mongoimport --host localhost --db WISE --collection geojson --mode=upsert --type json --file /docker-entrypoint-initdb.d/geojson.json --jsonArray
